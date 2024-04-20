702b1a138fb4fcdaf5b6efc1e917708e05d227f5
ayokunlejames <129544207+ayokunlejames@users.noreply.github.com>
2024-04-20 22:15:19 +0100

    Add files via upload

---
+++ nhspim.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+
+
+# In[ ]:
+
+
+connections.mysql]
+dialect = "mysql"
+host = "localhost"
+port = 3306
+database = "xxx"
+username = "xxx"
+password = "xxx"
+
